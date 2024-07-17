c51b43ff0f782b748008478227df6e362ee51263
Bil-26 <6278945@bdeb.qc.ca>
2024-07-17 15:55:21 -0400

    c est la version 3

--- test01_git.py
+++ test01_git.py
@@ -1,3 +1,11 @@
 print('Hello')
 for i in range(1000):
     print(f"la valeur de {i} est {i}")
+
+    print(f"la valeur de i est {i}")
+
+tab=[10,20,30,40,50]
+for x in range(4):
+    print(tab[x])
+
+    
